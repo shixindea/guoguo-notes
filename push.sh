922e0ac594f3a674654edcdabb97e8a116036a60
袁果锅 <2914310412@qq.com>
2024-11-21 16:26:22 +0800

    update:2024-11-21 16:26:21更新文档

--- push.sh
+++ push.sh
@@ -4,6 +4,7 @@ git add .
 
 git commit -m "update:$(date +%F%n%T)更新文档"
 
+echo "😀😀😀💨💨💨 update:$(date +%F%n%T)更新文档"
 # git commit -m "feat:搜索框插件$(date +%F%n%T)"
 
 git push && echo "Push 成功！" || echo "Push 失败！"
